0a86b7a5a70bc206f873b82b17376dc9787183c5
Durga-velu <durgadevi134babu@gmail.com>
2025-04-17 08:50:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // Your code here.
+let element
+let cubes=document.qurySelectorAll(".item")
+cubes.forEach((cube)=>{
+
+	cube.addEventListener("mousedown",(e)=>{
+       cube.style.position="absolute"
+		element=cube;
+		document.mousemove=(e)=>{
+			let x=e.pageX;
+			let y=e.pageY;
+			element.style.left=x -50 + "px";
+		element.style.right=y -50 + "px";
+		}
+		
+	})
+})
+
+document.mouseup=(e)=>{
+	element=null;
+}
+
+
+
+
+
+
+
+
+
+
+
+
